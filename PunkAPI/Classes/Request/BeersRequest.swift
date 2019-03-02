585c6b51c3acf5eae0ec5ead91e30e28581c77a6
Andrea Altea <oni.zerone@gmail.com>
2019-03-02 14:44:48 +0100

    Feat: define beer request

---
+++ PunkAPI/Classes/Request/BeersRequest.swift
@@ -0,0 +1,26 @@
+//
+//  BeersRequest.swift
+//  PunkAPI
+//
+//  Created by Andrea Altea on 25/02/2019.
+//
+
+import UIKit
+
+class BeersRequest: Request {
+
+    var page: Int
+    
+    init(page: Int = 0) {
+        self.page = page
+        
+    }
+    
+}
+
+extension BeersRequest: Request {
+    
+    var path: String {
+        return "beers"
+    }
+}
