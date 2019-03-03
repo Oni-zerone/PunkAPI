d6ffcbe51b48063b2d59d46e37bd3616880117f6
Andrea Altea <oni.zerone@gmail.com>
2019-03-03 18:57:12 +0100

    Feat: add tests

--- Example/Tests/PunkAPIInterfaceTests.swift
+++ Example/Tests/PunkAPIInterfaceTests.swift
@@ -9,6 +9,10 @@
 import XCTest
 @testable import PunkAPI
 
+enum TestError: Error {
+    case unknownError
+}
+
 class PunkAPIInterfaceTests: XCTestCase {
 
     var mockSession: MockURLSession!
@@ -30,13 +34,58 @@ class PunkAPIInterfaceTests: XCTestCase {
         mockSession.check.urlCheckBlock = { url in
             XCTAssert("https://api.test.it/v2/beers/1" == url.absoluteString)
         }
-        mockSession.check.responseConfig = (nil, nil, APIError.emptyResponse)
+        mockSession.check.responseConfig = (nil, nil, TestError.unknownError)
         
         interface.get(BeerRequest(id: 1)) { beerResult in
             switch beerResult {
             
             case let .failure(error):
+                XCTAssert(TestError.unknownError == error as? TestError)
+                
+            default:
+                XCTFail("wrong response")
+            }
+        }
+    }
+    
+    func testEmptyData() {
+        
+        mockSession.check.urlCheckBlock = { url in
+            XCTAssert("https://api.test.it/v2/beers/1" == url.absoluteString)
+        }
+        mockSession.check.responseConfig = (nil, nil, nil)
+        
+        interface.get(BeerRequest(id: 1)) { beerResult in
+            switch beerResult {
+                
+            case let .failure(error):
                 XCTAssert(APIError.emptyResponse == error as? APIError)
+                
+            default:
+                XCTFail("wrong response")
+            }
+        }
+    }
+
+    func testValidResponse() {
+
+        mockSession.check.urlCheckBlock = { url in
+            XCTAssert("https://api.test.it/v2/beers/1" == url.absoluteString)
+        }
+        
+        guard let beerData = BeerStub.multipleFirst.data(using: .utf8) else {
+            XCTFail("Unable to define beerData")
+            return
+        }
+        mockSession.check.responseConfig = (beerData, nil, nil)
+        
+        interface.get(BeerRequest(id: 1)) { beerResult in
+            switch beerResult {
+                
+            case let .success(beers):
+                
+                let decodedBeers = try? JSONDecoder().decode([Beer].self, from: beerData)
+                XCTAssert(beers == decodedBeers)
                 
             default:
                 XCTFail("wrong response")
