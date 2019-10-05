d30d80e48aad714b643436380fb4641ee67b5db6
Andrea Altea <oni.zerone@gmail.com>
2019-10-05 13:06:55 +0200

    Feat: prepare RxSwift extension files

--- Example/Pods/Target Support Files/Pods-PunkAPI_Example/Pods-PunkAPI_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-PunkAPI_Example/Pods-PunkAPI_Example-frameworks.sh
@@ -163,10 +163,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PunkAPI/PunkAPI.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/PromiseKit/PromiseKit.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/PunkAPI/PunkAPI.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/RxSwift/RxSwift.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
---
+++ PunkAPI/RxSwift/Classes/PunkAPI+RxSwift.swift
@@ -0,0 +1,12 @@
+//
+//  PunkAPI+RxSwift.swift
+//  Pods
+//
+//  Created by Andrea Altea on 05/10/2019.
+//
+
+import RxSwift
+import PunkAPI
+
+
+
