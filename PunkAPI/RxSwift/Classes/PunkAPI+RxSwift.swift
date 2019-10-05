7f6fe1d52ef78cc796d6533404ff5cf772dd04a0
Andrea Altea <oni.zerone@gmail.com>
2019-10-05 13:11:49 +0200

    Feat: add RxSwift extension to PunkApi

--- PunkAPI/RxSwift/Classes/PunkAPI+RxSwift.swift
+++ PunkAPI/RxSwift/Classes/PunkAPI+RxSwift.swift
@@ -5,8 +5,28 @@
 //  Created by Andrea Altea on 05/10/2019.
 //
 
+import Foundation
 import RxSwift
-import PunkAPI
 
+extension PunkApi {
+    
+    var rx: Reactive<PunkApi> {
+        return Reactive(self)
+    }
+}
 
-
+extension Reactive where Base: PunkApi {
+    
+    func get(_ request: PunkAPI.Request) -> Observable<[PunkAPI.Beer]> {
+        return Observable<[PunkAPI.Beer]>.create { observer in
+            let task = self.base.get(request) { result in
+                do {
+                    observer.onNext(try result.get())
+                } catch let error {
+                    observer.onError(error)
+                }
+            }
+            return Disposables.create(with: task?.cancel ?? {})
+        }
+    }
+}
